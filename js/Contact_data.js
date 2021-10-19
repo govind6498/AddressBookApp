cb27351894821bc11af00be173d4295370028360
govind6498 <govindsingh6498@gmail.com>
2021-10-20 00:39:13 +0530

    Added:Contact details class having getters and setters

---
+++ js/Contact_data.js
@@ -0,0 +1,70 @@
+class Contact{
+    get id(){
+        return this._id;
+    }
+    set id(id){
+        this._id = id;
+    }
+
+    get name(){
+        return this._name;
+    }
+    set name(name){
+        const nameRegex = RegExp("^[A-Z][a-zA-Z]{2,}(\\s{1}[A-Z][a-zA-Z]*)*$");
+        if(nameRegex.test(name)){
+            this._name = name;
+        }
+        else {
+            throw "Name is Invalid";
+        }
+    }
+    get phoneNumber(){
+        return this._phoneNumber;
+    }
+    set phoneNumber(phoneNumber){
+        const phoneNumberRegex = RegExp("^([+]?([0-9]{2}\\s{1})?[7-9]{1}[0-9]{9})$");
+        if(phoneNumberRegex.test(phoneNumber)){
+            this._phoneNumber = phoneNumber
+        }
+        else {
+            throw " Phone Number is Invalid"
+        }
+    }
+    get address(){
+        return this._address;
+    }
+    set address(address){
+        const addressRegex = RegExp('^([a-zA-Z0-9-:#$]{3,}\\s{0,1})+$');
+        if(addressRegex.test(address)){
+            this._address = address;
+        }
+        else{
+            throw "Address is Invalid";
+        }
+    }
+    get city(){
+        return this._city;
+    }
+    set city(city){
+        this._city = city;
+    }
+    get state(){
+        return this._state = state;
+    }
+    get zip(){
+        return this._zip
+    }
+    set zip(zip){
+        const zipRegex = RegExp("^[0-9]{6}$");
+        if(zipRegex.test(zip)){
+            this._zip = zip
+        }
+        else{
+            throw "Zip is Invalid";
+        }
+    }
+    toString(){
+        // return `id ${this.id} \nName ${this.name} \nPhone Number ${this.phoneNumber} \nAddress ${this.address} \nCity ${this.city} \nState ${this.state} \nZip  ${this.zip}`;
+        return "id="+this.id+" Name="+this.name+" Phone Number="+this.phoneNumber+" Address="+this.address+" City="+this.city+" State="+this.state+" zip="+this.zip;
+    }
+}
